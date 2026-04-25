9ccac5a1285b49871a9ce33b059419ef7d6f6797
Artem Maksimenko <mak5imenkoart@yandex.ru>
2026-04-25 14:19:51 +0300

    add option "format" in App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("info.picocli:picocli:4.7.7")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(
+        name = "gendiff",
+        version = "gendiff 1.0",
+        mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.\n" +
+                "filepath1         path to first file\n" +
+                "filepath2         path to second file")
+public class App implements Runnable {
+
+    @Option(names = { "-f", "--format" },
+            description = "output format [default: stylish]",
+            paramLabel = "format")
+    private String format = "format";
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public void run() {
+
     }
 }
